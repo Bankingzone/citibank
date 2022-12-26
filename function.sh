e96334f10a38f63facea7bfde8eaaef2a00a430c
Bhuvanabhuvi <bhuvanamohan8185@gmail.com>
2022-12-26 23:07:09 +0000

    update the file

---
+++ function.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Function are very imp.."
+greetfn()
+{
+echo "Hello everyone"
+echo "Good morning everyone"
+echo "Functions are important in code.."
+}
+echo "calling the function.."
+greetfn
