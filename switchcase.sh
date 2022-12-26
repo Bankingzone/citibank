b2fea5b26821c224cf4a25dee157c02a13a32bfb
Bhuvanabhuvi <bhuvanamohan8185@gmail.com>
2022-12-26 23:04:42 +0000

    updated

---
+++ switchcase.sh
@@ -0,0 +1,27 @@
+echo "Please enter the numbers from 1 to 5"
+read number
+case $number in
+1)
+echo "You have typed one"
+echo "Typed the correct number.."
+;;
+2)
+echo "You have typed two"
+echo "Typed the correct number.."
+;;
+3)
+echo "You have typed three"
+echo "Typed the correct number.."
+;;
+4)
+echo "You have typed four"
+echo "Typed the correct number.."
+;;
+5)
+echo "You have typed five"
+echo "Typed the correct number.."
+;;
+*)
+echo "INVALID NUMBER"
+echo "Please enter the number from 1 to 5 only"
+esac
